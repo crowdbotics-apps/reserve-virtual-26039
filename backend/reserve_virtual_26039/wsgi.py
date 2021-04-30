dfd521c4c203118d6b247ca1acc2ddd04baf446a
Crowdbotics <team@crowdbotics.com>
2021-04-30 08:17:25 +0000

    Update models

--- backend/reserve_virtual_26039/wsgi.py
+++ backend/reserve_virtual_26039/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reserve_virtual_26039.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reserve_virtual_26039.settings")
 
 application = get_wsgi_application()
